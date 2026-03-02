5aa9f6b703b6cf5d3c308e60946a35a3dc9b3b0a
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-02 08:17:27 +0530

    commit

---
+++ user input/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        // nextInt()  -- integer type
+        // nextFloat() -- decimal number type 
+        // nextDouble
+        // so more 
+        System.out.println(name);   
+        
+    }
+}
