e58e1143efedf14f0e2c99537bffff2b5d4ff694
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-13 08:55:19 +0530

    commit

--- conditional Statements/ifelse.java
+++ conditional Statements/ifelse.java
@@ -1,23 +1,23 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class ifelse {
+public class ifelse {
 
 
-//     public static void main (String args []){
+    public static void main (String args []){
 
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter your first value");
-//         int a = sc.nextInt();
-//         System.out.println("Enter your 2nd value");
-//         int b = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first value");
+        int a = sc.nextInt();
+        System.out.println("Enter your 2nd value");
+        int b = sc.nextInt();
 
-//         if (a == b){
-//             System.out.println("equal");
-//         } else if (a > b) {
-//             System.out.println(" a is greater ");
+        if (a == b){
+            System.out.println("equal");
+        } else if (a > b) {
+            System.out.println(" a is greater ");
             
-//         } else { 
-//             System.out.println(" a is lessthan");
-//         }
-//     }
-// }
+        } else { 
+            System.out.println(" a is lessthan");
+        }
+    }
+}
