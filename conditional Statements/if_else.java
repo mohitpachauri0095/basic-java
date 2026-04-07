2ce6785bbb3d3d17852466555c9f86a8da1b5337
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-04-07 19:10:18 +0530

    commit

--- conditional Statements/if_else.java
+++ conditional Statements/if_else.java
@@ -6,7 +6,7 @@ public class if_else {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
 
-        if (age > 18){
+        if (age > 12){
             System.out.println("adult");
         }else {
             System.out.println("not adult");
